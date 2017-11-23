94b4ca9d79b7f7eced741fab6e3f67633e8b2e33
seeiceland <32713370+seeiceland@users.noreply.github.com>
2017-11-23 10:02:20 +0200

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,29 +1,21 @@
-console.log("Up and running!");
+var cards = ['queen','queen','king','king'];
 
-var cards = ["queen","queen","king","king"];
+ var cardsInPlay = [];
 
-var cardsInPlay = [];
+ var cardOne = cards[0]
+ cardsInPlay.push(cardOne);
+ console.log("User flipped queen");
 
-var cardOne = cards[0];
+ var cardTwo = cards[2];
+ cardsInPlay.push(cardTwo);
 
-cardsInPlay.push('cardOne');
+ console.log("User flipped king");
 
-var cardTwo = cards[2];
+ if (cardsInPlay.length === 2){
+   if (cardsInPlay[0] === cardsInPlay[1]){
+     alert("You found a match!");
+   }else{
+     alert("Sorry, try again");
 
-cardsInPlay.push('cardTwo');
-
-
-
-console.log("User flipped " + cardsInPlay[0]);
-
-console.log("User flipped " + cardsInPlay[1]);
-
-// if cardsInPlay.length===2 {
-// console.log("true");
-// }
-
-if cardsInPlay[0]===cardsInPlay[1] {
-alert("You found a match")
-} else  {
-console.log("Sorry, try again");
-}
+   }
+ }
