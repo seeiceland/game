40d03832b05c874ce036f49e51b397b335067a4e
seeiceland <32713370+seeiceland@users.noreply.github.com>
2017-11-23 10:09:53 +0200

    even more game logic
    even more game logic

---
+++ memory_game/js/main2.js
@@ -0,0 +1,81 @@
+console.log("Up and running!");
+
+var cards = ["queen","queen","king","king"];
+
+var cardsInPlay = [];
+
+//var flipCard = function(
+// if cardsInPlay.length===2 {
+ console.log("User flipped" + cards[cardId]);
+// }
+
+//if cardsInPlay[0]===cardsInPlay[1] {
+//alert("You found a match")
+//} else  {
+//console.log("Sorry, try again");
+//}
+
+var cards = [
+{
+rank: "queen",
+suit: '"hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: '"diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: '"hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: '"diamonds",
+cardImage: "images/king-of-diamonds.png",
+},
+];
+
+console.log("User flipped " + cards[cardId].rank);
+
+cardsInPlay.push(cards[cardId].rank);
+
+console.log()
+
+console.log()
+
+
+(cards[cardId])
+
+//firstName, lastName, year, city) {
+//console.log("Hello " + firstName + " " + lastName + " born in "+ year + " from " + city + "!" );
+//};
+
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+
+
+console.log("User flipped " + cardsInPlay[0]);
+
+console.log("User flipped " + cardsInPlay[1]);
+
+if cardsInPlay.length===2 {
+console.log("true");
+}
+
+if cardsInPlay[0]===cardsInPlay[1] {
+alert("You found a match")
+} else  {
+console.log("Sorry, try again");
+}
+
+
