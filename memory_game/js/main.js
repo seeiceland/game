692fe0e56338a971ab493e2959424f281aea3761
seeiceland <32713370+seeiceland@users.noreply.github.com>
2017-11-23 10:17:05 +0200

    more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,36 @@
 var cards = ['queen','queen','king','king'];
 
- var cardsInPlay = [];
+var cardsInPlay = [];
 
- var cardOne = cards[0]
- cardsInPlay.push(cardOne);
- console.log("User flipped queen");
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    console.log('You found a match!');
+    } else {
+      console.log('Sorry, try again.');
+    }
+  }
+}
 
- var cardTwo = cards[2];
- cardsInPlay.push(cardTwo);
+var flipCard = function (cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
 
- console.log("User flipped king");
+  if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+      alert("You found a match!");
+    } else {
+    alert("Sorry, try again");
+    }
+  }
+  checkForMatch();
 
- if (cardsInPlay.length === 2){
-   if (cardsInPlay[0] === cardsInPlay[1]){
-     alert("You found a match!");
-   }else{
-     alert("Sorry, try again");
+}
 
-   }
- }
+
+
+flipCard(0);
+flipCard(2);
+
+
+
+console.log("Up and running!")
