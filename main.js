40de43bb684ac1dedd0b1bb1838babade23fa095
seeiceland <32713370+seeiceland@users.noreply.github.com>
2017-11-23 10:20:10 +0200

    even more game logic

--- main.js
+++ main.js
@@ -1,37 +1,79 @@
+console.log("Up and running!");
 
-var cards = ['queen','queen','king','king'];
+var cards = ["queen","queen","king","king"];
 
 var cardsInPlay = [];
 
-var checkForMatch = function() {
-  if (cardsInPlay[0] === cardsInPlay[1]){
-    console.log('You found a match!');
-    } else {
-      console.log('Sorry, try again.');
-    }
-  }
+//var flipCard = function(
+// if cardsInPlay.length===2 {
+ console.log("User flipped" + cards[cardId]);
+// }
+
+//if cardsInPlay[0]===cardsInPlay[1] {
+//alert("You found a match")
+//} else  {
+//console.log("Sorry, try again");
+//}
+
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",
+},
+];
+
+console.log("User flipped " + cards[cardId].rank);
+
+cardsInPlay.push(cards[cardId].rank);
+
+console.log()
+
+console.log()
+
+
+(cards[cardId])
+
+//firstName, lastName, year, city) {
+//console.log("Hello " + firstName + " " + lastName + " born in "+ year + " from " + city + "!" );
+//};
+
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+
+
+console.log("User flipped " + cardsInPlay[0]);
+
+console.log("User flipped " + cardsInPlay[1]);
+
+if cardsInPlay.length===2 {
+console.log("true");
 }
 
-var flipCard = function (cardId){
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
-
-  if (cardsInPlay.length === 2){
-    if (cardsInPlay[0] === cardsInPlay[1]){
-      alert("You found a match!");
-    } else {
-    alert("Sorry, try again");
-    }
-  }
-  checkForMatch();
-
+if cardsInPlay[0]===cardsInPlay[1] {
+alert("You found a match")
+} else  {
+console.log("Sorry, try again");
 }
-
-
-
-flipCard(0);
-flipCard(2);
-
-
-
-console.log("Up and running!")
